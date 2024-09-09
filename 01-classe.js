819b4c5b83a22c0d024f5f1dc0575552a62fd275
João Neves <contatojotaico@gmail.com>
2024-09-09 08:47:29 -0400

    poo classe

---
+++ 01-classe.js
@@ -0,0 +1,8 @@
+class Pessoa {
+    constructor(nome, idade, altura, peso) {
+        this.nome = nome;
+        this.idade = idade;
+        this.altura = altura;
+        this.peso = peso;
+    }
+}
