226ad1ff432e3215117c0b432806f22c715cb16f
João Neves <contatojotaico@gmail.com>
2024-09-30 11:05:09 -0400

    add criando instâncias e testando comportamento

--- 19-classe-abstrata-simulado-em-javascript.js
+++ 19-classe-abstrata-simulado-em-javascript.js
@@ -33,3 +33,33 @@ class Guerreiro extends Personagem {
         console.log(`${this.nome} levanta o escudo para se defender`);
     }
 }
+// Classe derivada "Mago"
+class Mago extends Personagem {
+    constructor(nome, magia) {
+        super(nome);
+        this.magia = magia;
+    }
+
+    // implementação dos métodos abstratos da classe Personagem
+    atacar() {
+        console.log(`${this.nome} lança a magia ${this.arma}`);
+    }
+
+    defender() {
+        console.log(`${this.nome} usa um escudo mágico para se defender`);
+    }
+}
+// Criando instância e testando o comportamento
+try {
+    const Personagem = new Personagem("Invalido"); // Isso vai lançar um erro
+}   catch (error) {
+    console.log(error.message); // "Não é possível instaciar a classe abstrata Personagem diretamente."
+}
+
+const guerreiro = new Guerreiro("Thorin", "Espada");
+guerreiro.atacar(); // "Thorin ataca com a Espada!"
+guerreiro.defender(); // "Thorin levanta o escudo para se defender!"
+
+const mago = new Mago("Gandalf", "Bola de Fogo");
+mago.atacar(); // "Gandalf lança a magia Bola de Fogo!"
+mago.defender(); // "Gandalf usa um escudo mágico para se defender!"
